9906506cc1a60dd34b2f64d28cc7378e7b3c927c
Brayan Camilo Joya Herrea <b.joya@uniandes.edu.co>
2024-11-18 01:13:38 -0500

    Termocuple code

---
+++ Code/src/pins.h
@@ -0,0 +1,17 @@
+#ifndef PINS_H
+#define PINS_H
+
+#include <Arduino.h>
+
+/**
+ * @brief Pines de conexión a ESP 32.
+ *
+ * Se definen los pines de las conexiones para los diferentes dispositivos.
+ */
+#define thermoDO  23
+#define thermoCS  5
+#define thermoCLK  18
+
+
+
+#endif 
